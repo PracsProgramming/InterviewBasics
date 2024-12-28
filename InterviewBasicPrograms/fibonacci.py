d1e9121a935d8da6a5e9c8a6d80c7a9c423efa2b
PracsProgramming <kulkarniprachi@outlook.com>
2024-12-28 14:25:01 +0530

    Update fibonacci.py

--- InterviewBasicPrograms/fibonacci.py
+++ InterviewBasicPrograms/fibonacci.py
@@ -1,11 +1,11 @@
-n = 10
-i = 0
-j = 1
-k = j
+n = 10 #fibonacci of 1st 10 numbers
+i = 0 #start number from 0
+j = 1 #second number
+k = j # sum of 1st and 2nd number
 count = 1
 while count <10:
-    print(i)
-    count += 1
-    i = j
-    j = k
-    k = i + j
+    print(i) #print series from 0
+    count += 1 #increment count
+    i = j # start number increment
+    j = k 
+    k = i + j  # 2nd number will be sum of previous 2
